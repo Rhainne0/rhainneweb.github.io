87845b11a9dba8d54d6057272dafb3be241f60f1
Rhainne0 <110919937+Rhainne0@users.noreply.github.com>
2022-12-30 15:28:18 +0800

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,26 @@
+let LastImage = document.getElementsByClassName("focus2-img")[0];
+let MidImage = document.getElementsByClassName("bush1-img")[0];
+let FrontImage = document.getElementsByClassName("bush2-img")[0];
+let Historical = document.querySelector("#landing h1");
+let GREECE = document.querySelector("#landing h2");
+let Developer = document.querySelector("#landing h3");
+let Button = document.querySelector(".btn");
+
+window.addEventListener('scroll', function () {
+    let value = window.scrollY;
+
+    MidImage.style.left = value * 0.9 + 'px';
+    
+
+    FrontImage.style.right = value * 0.8 + 'px';
+    
+
+    LastImage.style.top = value * 0.8 + 'px';
+
+    Historical.style.right = value * 0.5 + 'px';
+    Historical.style.marginTop = value * 0.3 + 'px';
+    Developer.style.left = value * 0.5 + 'px';
+    Developer.style.marginTop = value * 0.3 + 'px';
+    Button.style.marginTop = value * 0.1 + 'px';
+    
+})
